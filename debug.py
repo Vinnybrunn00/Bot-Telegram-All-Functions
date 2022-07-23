f3b65ae464d6a370ce285162e9dab57d81a08ed0
Vinícius Bruno <91799009+Vinnybrunn00@users.noreply.github.com>
2022-07-23 13:26:44 -0300

    Update debug.py

--- debug.py
+++ debug.py
@@ -4,26 +4,37 @@ from rich import print
 import pyfiglet as pg
 
 bot = TeleBot(TeleToken())
-banner = pg.figlet_format('TeleBot')
+banner = pg.figlet_format('HubberBot')
 
 def Help(message):
     bot.send_message(message.chat.id, '''
         -------------------- ☾ 🤖 *COMMANDS MENU* 🤖☽ --------------------
-
         *!scan* -> Extract text from an image
                   _exemple_ : *!scan + imagem*
-
         *!qrcode* -> Returns image of the qrcode through the link
                   _exemple_ : *!qrcode + link*
-
         *!creator* -> Return developer contact
-
         ''', parse_mode='Markdown'
                 )
 
 def Connected():
-    print(f'[cyan][bold]{banner}[/]')
+    print(f'[bold]{banner}[/]')
     print(f'[blue]:heavy_check_mark: Connected!\n')
+
+
+def Welcome_():
+    print(
+        '-------------------------------------'
+        '\nMethod: [purple]Welcome[/]\n'
+                )
+
+def Delallmessage(message):
+    print(
+        '-------------------------------------'
+        f'\nUsername: [cyan]{message.from_user.first_name}[/]\n'
+        f'Message: [blue][italic]{message.text}[/]\n'
+        f'Method: [purple]Delete Message[/]\n'
+            )
 
 def Scan(message):
     print(
